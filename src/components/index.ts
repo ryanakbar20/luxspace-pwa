bb42fe0a944f848c6bd6b2742a168de1dc37882b
ryanakbar20 <rian.akbar@majoo.id>
2023-04-06 05:28:07 +0700

    Init luxspace project

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Header } from "./Header";
+export { default as Hero } from "./Hero";
+export { default as Browse } from "./Browse";
+export { default as Arrived } from "./Arrived";
+export { default as Client } from "./Client";
+export { default as Aside } from "./Aside";
+export { default as Footer } from "./Footer";
